dd1c05a3027df0d5752f6488935ab5f1c279338a
rainbee2214 <rainbee2214@gmail.com>
2015-04-28 23:09:35 -0300

    Added menu scene and level loader and some UI.

---
+++ SpaceHackers/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string level = "Menu";
+    public bool clickToLoad = true;
+    
+    void Update()
+    {
+        if (clickToLoad && Input.GetButtonDown("Action")) LoadLevel(level);
+    }
+
+    public void LoadLevel(string l)
+    {
+        Application.LoadLevel(l);
+    }
+}
