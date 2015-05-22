714fad0ba8d5051332b6dc9e57efe9fb6adc8503
rainbee2214 <rainbee2214@gmail.com>
2015-05-21 21:19:06 -0300

    Added a base enemy class.

---
+++ SpaceHackers/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseEnemy : MonoBehaviour
+{
+    string enemyName;
+    public string EnemyName
+    {
+        get { return enemyName; }
+        set { enemyName = value; }
+    }
+
+    int level; //Current level of the enemy
+    public int Level
+    {
+        get { return level; }
+        set { level += value; }
+    }
+
+    int experience; //Experience received from the enemy
+    public int Experience
+    {
+        get { return experience; }
+        set { experience = value; }
+    }
+}
