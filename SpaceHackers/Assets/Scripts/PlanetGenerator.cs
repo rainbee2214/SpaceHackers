1dc2dfc6df7100bf97af587be4524d6d0f8f648e
rainbee2214 <rainbee2214@gmail.com>
2015-05-23 11:31:44 -0300

    Added planet generation for each star.

---
+++ SpaceHackers/Assets/Scripts/PlanetGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    static List<GameObject> planets;
+
+    public static void LoadPlanets()
+    {
+        planets = new List<GameObject>();
+        int i = 1;
+        while (i <= 50)
+        {
+            planets.Add(Resources.Load("Prefabs/Planets/Planet" + i++, typeof(GameObject)) as GameObject);
+        }
+        Debug.Log(planets[23].name);
+    }
+
+    public static GameObject GetPlanet()
+    {
+        return planets[Random.Range(0, planets.Count)];
+    }
+}
---
+++ SpaceHackers/Assets/Scripts/Star.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Star : MonoBehaviour
+{
+    float radius;
+    int numberOfPlanets;
+
+    List<GameObject> planets;
+
+    void Awake()
+    {
+        //Get the star radius from the collider
+        radius = GetComponent<CircleCollider2D>().radius * 50;
+        GetComponent<CircleCollider2D>().radius = (radius / 50) * 1.25f;
+        SetupStar();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void SetupStar()
+    {
+        numberOfPlanets = (int)radius / 18;
+        GetPlanets();
+    }
+
+    void GetPlanets()
+    {
+        planets = new List<GameObject>();
+        PlanetGenerator.LoadPlanets();
+
+        Vector3 location = Vector3.zero;
+        float r = 0f;
+
+        for (int i = 0; i < numberOfPlanets; i++)
+        {
+            r = Random.Range(radius/50, radius/5);
+            location.Set(r, location.y, -1f);
+            planets.Add(Instantiate(PlanetGenerator.GetPlanet(), location, Quaternion.identity) as GameObject);
+            planets[i].transform.SetParent(transform);
+            planets[i].name = "Planet" + i;
+        
+        }
+    }
+}
