a3aabc789021fb22f58b923c9b23eb7f686ee337
rainbee2214 <rainbee2214@gmail.com>
2015-06-04 21:33:30 -0300

    Added the collectable class and space junk.

---
+++ SpaceHackers/Assets/Scripts/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collectable : MonoBehaviour 
+{
+    public virtual void Collect()
+    {
+        Debug.Log("Collectable :)" + this.name);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Collect();
+        }
+    }
+}
---
+++ SpaceHackers/Assets/Scripts/SpaceJunk.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceJunk : Collectable
+{
+    public override void Collect()
+    {
+        //base.Collect();
+        Debug.Log("SpaceJunk!" + this.name);
+
+        //Turn off and return yourself to the object pool of collectables.
+    }
+}
