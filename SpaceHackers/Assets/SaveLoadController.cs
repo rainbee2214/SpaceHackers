97d2a55afd54510176ad6e3b1fdd8349af42133c
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-06-07 17:41:11 -0300

    Finish UI design and start UI logic

---
+++ SpaceHackers/Assets/SaveLoadController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveLoadController
+{
+    public static void Create(int slot)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/SpaceHackersSaveSlot" + slot + ".dat");
+        
+        SaveGame data = new SaveGame();
+        bf.Serialize(file, data);
+
+        file.Close();
+    }
+
+    public static void Save(int slot, SaveGame theData)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/SpaceHackersSaveSlot" + slot + ".dat", FileMode.Open);
+
+        bf.Serialize(file, theData);
+
+        file.Close();
+    }
+
+    public static SaveGame Load(int slot)
+    {
+        if (File.Exists(Application.persistentDataPath + "/SpaceHackersSaveSlot" + slot + ".dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/SpaceHackersSaveSlot" + slot + ".dat", FileMode.Open);
+            SaveGame data = (SaveGame)bf.Deserialize(file);
+            file.Close();
+            return data;
+        }
+        else return null;
+    }
+
+    public static bool CheckForSaves()
+    {
+        for (int i=1; i<=4; i++)
+        {
+            if (File.Exists(Application.persistentDataPath + "/SpaceHackersSaveSlot" + i + ".dat")) return true;
+        }
+        return false;
+    }
+}
+
+[Serializable]
+public class SaveGame
+{
+    public string name = "";
+    public int playerRace = 0;
+    public int playerClass = 0;
+    public int playerShip = 0;
+
+    public int health = 0;
+    public int experience = 0;
+}
---
+++ SpaceHackers/Assets/TitleController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour
+{
+    // Canvas references
+    public GameObject titleCanvas;
+    public GameObject loadCanvas;
+    public GameObject nameCanvas;
+    public GameObject raceCanvas;
+    public GameObject classCanvas;
+    public GameObject shipCanvas;
+    public GameObject confirmCanvas;
+
+    void Start()
+    {
+        // Disable all canvases but title
+        titleCanvas.SetActive(true);
+        loadCanvas.SetActive(false);
+        nameCanvas.SetActive(false);
+        raceCanvas.SetActive(false);
+        classCanvas.SetActive(false);
+        shipCanvas.SetActive(false);
+        confirmCanvas.SetActive(false);
+
+
+        // If any saved games
+        if (SaveLoadController.CheckForSaves())
+        {
+            //Enable load button
+        }                
+    }
+}
