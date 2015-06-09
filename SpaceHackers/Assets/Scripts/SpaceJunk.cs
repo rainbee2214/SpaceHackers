b8cb5dce3e623e1a4f7145d5920fbe68a0b21aa0
rainbee2214 <rainbee2214@gmail.com>
2015-06-08 22:10:27 -0300

    Added a more robust space junk.

--- SpaceHackers/Assets/Scripts/SpaceJunk.cs
+++ SpaceHackers/Assets/Scripts/SpaceJunk.cs
@@ -1,8 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpaceJunk : Collectable
 {
+    SpriteRenderer spriteRenderer;
+    public List<Sprite> sprites;
+
+    public void Start()
+    {
+        SetSprite();
+        
+    }
+
+    void SetSprite()
+    {
+        sprites = new List<Sprite>();
+        for (int i = 0; i < 7; i++ ) //Doesn't like this path :(
+            sprites.Add(Resources.Load("Sprites/Resource/junk" + i, typeof(Sprite)) as Sprite);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        gameObject.SetActive(false);
+    }
+    
     public override void Collect()
     {
         //base.Collect();
